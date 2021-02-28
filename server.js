46abc386c445c362ef21d0bb455f1eede11be991
iputuagussetiawan <agussetiawaniputu@gmail.com>
2021-02-28 14:03:34 +0800

    add backend modul upload

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(
 //Routes
 app.use("/user", require("./routes/userRouter"));
 app.use("/api", require("./routes/categoryRouter"));
+app.use("/api", require("./routes/upload"));
 
 //connect to mongodb
 const URI = process.env.MONGODB_URL;
